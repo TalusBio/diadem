92d906ab28336d5a2bf466ea430dce224134ce0a
Sebastian Paez <sebastianpaez@Taluss-MacBook-Air.local>
2023-01-20 14:11:08 -0800

    added test using synthetic data

---
+++ tests/test_dia_search.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from ms2ml import Peptide
+
+from diadem.config import DiademConfig
+from diadem.search.diadem import diadem_main
+
+
+def test_dia_search_works(tmpdir, shared_datadir):
+    """Uses simulated data to test diadem.
+
+    Uses data simulated using Synthedia to check if the full search
+    can recall all peptides from FGFR1 in the simulated dataset.
+    """
+    fasta = shared_datadir / "mzml/P11362.fasta"
+    mzml = shared_datadir / "mzml/FGFR1_600_800_5min_group_0_sample_0.mzML"
+    config = DiademConfig()
+    out = str(tmpdir / "out")
+    diadem_main(config=config, mzml_path=mzml, fasta_path=fasta, out_prefix=out)
+
+    expected_csv = out + ".diadem.csv"
+    df = pd.read_csv(expected_csv)
+    peptides = {Peptide.from_sequence(x).stripped_sequence for x in df.Peptide.unique()}
+
+    theo_table = pd.read_csv(
+        shared_datadir / "mzml/FGFR1_600_800_5min_peptide_table.tsv", sep="\t"
+    )
+    theo_table = theo_table[
+        theo_table["MS2 chromatographic points group_0_sample_0"] > 0
+    ]
+
+    theo_table["seen_seqs"] = [x in peptides for x in theo_table.Sequence]
+    missed_theo = theo_table[[not x for x in theo_table["seen_seqs"]]]
+    seen_theo = theo_table[list(theo_table["seen_seqs"])]
+
+    assert len(missed_theo) == 0
+    assert len(seen_theo) > 1
